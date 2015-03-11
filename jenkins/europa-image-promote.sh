42b53a2108bce5ec816bb019de4e731bad3956b1
Phil Lu <plu@zenoss.com>
2015-03-11 10:38:21 -0500

    add _phase to stable tag; e.g. zenoss/resmgr_5.0:5.0.0_2

--- jenkins/europa-image-promote.sh
+++ jenkins/europa-image-promote.sh
@@ -11,7 +11,8 @@ set -x
 repo_tag() {
     flavor="$1"        # examples: core, resmgr
     maturity="$2"      # examples: stable, testing, unstable
-    phase="$3"         # examples: BETA1, CR13, GA
+    phase="$3"         # examples for testing: BETA1, CR13, GA
+                       # examples for stable: 1, 2, 3
     # repo name: e.g., zenoss/resmgr_5.0
     repo="zenoss/${flavor}_${SHORT_VERSION}"
     case $maturity in
@@ -24,8 +25,8 @@ repo_tag() {
             tag="${VERSION}_${phase}"
             ;;
         stable )
-            # e.g., 5.0.0
-            tag="${VERSION}"
+            # e.g., 5.0.0_2
+            tag="${VERSION}_${phase}"
             ;;
         * )
             exit 1
@@ -42,7 +43,7 @@ case $VERSION in
             FROM_STRING=$(repo_tag "$FLAVOR" "$FROM_MATURITY" "$FROM_RELEASEPHASE")
             TO_STRING=$(repo_tag "$FLAVOR" "$TO_MATURITY" "$TO_RELEASEPHASE")
             docker pull "$FROM_STRING"
-            docker tag  "$FROM_STRING" "$TO_STRING"
+            docker tag -f "$FROM_STRING" "$TO_STRING"
             docker push "$TO_STRING"
         done
         ;;
