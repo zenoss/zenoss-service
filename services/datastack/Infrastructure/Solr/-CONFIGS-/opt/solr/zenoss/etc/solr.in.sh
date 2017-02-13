465c91a5b1468654552e05cd5151ac80e3209aa7
Ubuntu <ubuntu@ip-172-31-7-4.ec2.internal>
2017-02-13 17:04:11 +0000

    Add ZK_QUORUM variable in solr.in.sh. Change quorum variables to one item until we figure out why multiple does not work

--- services/datastack/Infrastructure/Solr/-CONFIGS-/opt/solr/zenoss/etc/solr.in.sh
+++ services/datastack/Infrastructure/Solr/-CONFIGS-/opt/solr/zenoss/etc/solr.in.sh
@@ -1,4 +1,4 @@
 # This file is injected by ControlCenter with container-specific parameters
-ZK_HOST=172.31.16.14:5181/solr,172.31.17.9:5181/solr,172.31.27.159:5181/solr
-ZK_QUORUM=172.31.16.14:5181,172.31.17.9:5181,172.31.27.159:5181
+ZK_HOST=172.31.16.14:5181/solr
+ZK_QUORUM=172.31.16.14:5181
 
