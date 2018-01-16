9c84812fff30b005bea0ce9b035412bd8ed74cd3
jplouis <jplouis@gmail.com>
2018-01-16 15:57:16 -0600

    add cse service defintions, initial version is copy of resgmr

---
+++ services/Zenoss.cse/Infrastructure/Solr/-CONFIGS-/opt/solr/zenoss/etc/solr.in.sh
@@ -0,0 +1,4 @@
+# This file is injected by ControlCenter with container-specific parameters
+# ZK_HOST={{with $zks := (child (child (parent .) "HBase") "ZooKeeper").Instances }}{{range (each $zks)}}127.0.0.1:{{plus 2181 .}}{{if ne (plus 1 .) $zks}},{{end}}{{end}}{{end}}/solr
+SOLR_JAVA_MEM="-Xmx{{.RAMCommitment}}"
+
