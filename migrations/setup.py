2e28fd9232c49b1a22c7dd41d9e9bdd2694f0d69
Jason Peacock <jpeacock@zenoss.com>
2020-04-17 09:21:06 -0500

    Add service migration package.
    The 'zenservicemigration' package is designed to run during the upgrade of a Zenoss system without relying on ZODB.
    
    The inaugural migration is to migrate the mariadb services to use the MariaDB 10.3 image.
    
    Fixes ZEN-32769.

---
+++ migrations/setup.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+_version = os.environ.get("VERSION")
+if _version is None:
+    print("VERSION environment variable not found", file=sys.stderr)
+    sys.exit(1)
+
+
+setup(
+    name="zenservicemigration",
+    version=_version,
+    description="Zenoss Service Definition Migration Scripts",
+    author="Zenoss, Inc.",
+    url="https://www.zenoss.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    package_data={
+        "zenservicemigration": ["migrations/*.py"],
+    },
+    zip_safe=False,
+    install_requires=["servicemigration"],
+    python_requires=">=2.7,<3",
+    entry_points={
+        "console_scripts": [
+            "zensvcmigrate=zenservicemigration.migrate:main",
+        ],
+    },
+)
